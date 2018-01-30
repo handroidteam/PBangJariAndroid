8857d4c31d16975dd2d250f4668576b8c0c7c9cf
DeityKJM <35284806+DeityKJM@users.noreply.github.com>
2018-01-30 16:01:35 +0900

    first

--- app/src/main/java/com/handroid/mytownamp/pbangjariandroid/Server/Pcbang_uri.java
+++ app/src/main/java/com/handroid/mytownamp/pbangjariandroid/Server/Pcbang_uri.java
@@ -8,4 +8,5 @@ public class Pcbang_uri {
     public static String pcBang_allceo= "http://13.124.1.90:3000/api/pcBang/getAll";
     public static String pcBang_search_id="http://13.124.1.90:3000/api/pcBang/getDetail/";
     public static String pcBang_map_range="http://13.124.1.90:3000/api/pcBang/getRange/";
+    public static String pcBang_map_info="http://13.124.1.90:3000/api/pcMap/getDetail/"; //+id
 }
