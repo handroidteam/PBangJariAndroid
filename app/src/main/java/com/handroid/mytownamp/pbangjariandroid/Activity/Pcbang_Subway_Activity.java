cc4a1da74eab7f1cba6ccd25eb903e853c40a69c
DeityKJM <35284806+DeityKJM@users.noreply.github.com>
2018-02-06 19:47:10 +0900

    Subway Activity

--- app/src/main/java/com/handroid/mytownamp/pbangjariandroid/Activity/Pcbang_Subway_Activity.java
+++ app/src/main/java/com/handroid/mytownamp/pbangjariandroid/Activity/Pcbang_Subway_Activity.java
@@ -1,7 +1,9 @@
 package com.handroid.mytownamp.pbangjariandroid.Activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,11 +11,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.handroid.mytownamp.pbangjariandroid.Common.Pcbang_uri;
+import com.handroid.mytownamp.pbangjariandroid.Common.Subway_info;
 import com.handroid.mytownamp.pbangjariandroid.PcbangArray.PcBang_info;
 import com.handroid.mytownamp.pbangjariandroid.PcbangArray.pcAdapter;
 import com.handroid.mytownamp.pbangjariandroid.R;
@@ -29,95 +34,77 @@ import java.util.ArrayList;
  * Created by Jeongmin on 2018-01-20.
  */
 
-public class Pcbang_Subway_Activity extends Activity {
+public class Pcbang_Subway_Activity extends Activity implements View.OnClickListener, ListView.OnItemClickListener {
 
     TextView btn_close, App_Title, btn_search_sub;
-
-    ArrayAdapter PcbangAdapter;
-    ArrayList<String> Pcinfo_arr = new ArrayList<>();
-    ListView pcbang_list;
-
     Context mContext;
+    ListView listView;
+    ArrayAdapter adapter;
+    ArrayList<String> strings = new ArrayList<>();
+    String[] data;
+    LinearLayout sub_lay1, sub_lay2;
+
+    private void SettingLayout() {
+        data = Subway_info.Lines;
+        Datainsert(data);
+
+        sub_lay1=(LinearLayout)findViewById(R.id.sub_lay1);
+        sub_lay2=(LinearLayout)findViewById(R.id.sub_lay2);
+
+        mContext = getApplicationContext();
+        btn_close = findViewById(R.id.btn_text_back);
+        App_Title = findViewById(R.id.text_title);
+        btn_search_sub = findViewById(R.id.btn_search_sub);
+        btn_close.setOnClickListener(this);
+        btn_search_sub.setOnClickListener(this);
+
+        listView = (ListView) findViewById(R.id.list);
+        adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, strings);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pcbang_subway_layout);
-        mContext = getApplicationContext();
-        btn_close = findViewById(R.id.btn_text_back);
-        App_Title = findViewById(R.id.text_title);
-        btn_search_sub = findViewById(R.id.btn_search_sub);
 
-        HttpRequest httpRequest = new HttpRequest(httpCallback);
-        httpRequest.execute(Pcbang_uri.pcBang_allceo);
+        SettingLayout();
+        Log.d("Sub_data", "oncreate");
 
-        pcbang_list = findViewById(R.id.loc_list);
-
-        PcbangAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Pcinfo_arr);
-
-
-        pcbang_list.setAdapter(PcbangAdapter);
-
-
-        pcbang_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-
-                //상세정보 액티비티
-              /*  Intent pcbang = new Intent(Pcbang_Subway_Activity.this, Pcbang_Detail_Activity.class);
-                pcbang.putExtra("pcbanginfo", Pcinfo_arr.get(position));//pc방 고유 코드
-                startActivity(pcbang);*/
-
-
-                HttpRequest httpRequest = new HttpRequest(httpCallbacks);
-                httpRequest.execute(Pcbang_uri.pcBang_map_info + Pcinfo_arr.get(position));
-
-            }
-        });
-        btn_close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-
-        btn_search_sub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(Pcbang_Subway_Activity.this, "미구현", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
 
-    HttpCallback httpCallback = new HttpCallback() {
-        @Override
-        public void onResult(String result) {
-            try {
-                Pcinfo_arr.clear(); //서버 데이터 통신
-                JSONArray root = new JSONArray(result);//즐겨찾기 데이터값
-                for (int i = 0; i < root.length(); i++) {
-                    Pcinfo_arr.add(root.getJSONObject(i).getString("_id"));
-                }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_text_back:
+                finish();
+                break;
+            case R.id.btn_search_sub:
+                Toast.makeText(Pcbang_Subway_Activity.this, "미구현", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 
+    public void Datainsert(String[] datas) {
+        for (int i = 0; i < datas.length; i++) {
+            strings.add(datas[i]);
+        }
+    }
 
-            } catch (JSONException d) {
-
-                d.printStackTrace();
-
-            } catch (NullPointerException f) {
-                Toast.makeText(Pcbang_Subway_Activity.this, "데이터 에러", Toast.LENGTH_SHORT).show();
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Log.d("Sub_data", "click list");
+        final String[] datas = new Subway_info().GetData(data[i]);
+        AlertDialog.Builder a = new AlertDialog.Builder(Pcbang_Subway_Activity.this);
+        a.setItems(datas, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.d("Sub_data", "alert click");
+                Toast.makeText(Pcbang_Subway_Activity.this, datas[i], Toast.LENGTH_SHORT).show();
             }
-            PcbangAdapter.notifyDataSetChanged();
-        }
-    };
+        });
+        a.show();
 
-    HttpCallback httpCallbacks = new HttpCallback() {
-        @Override
-        public void onResult(String result) {
-
-        }
-    };
-
-
+    }
 }
